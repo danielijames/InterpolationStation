864b5e4076747d6a5e34af8b9e27f68cc02f61bb
danielijames <45879477+danielijames@users.noreply.github.com>
2019-01-13 18:00:29 -0500

    Add files via upload

--- InterpolationStation.js
+++ InterpolationStation.js
@@ -13,13 +13,13 @@ document.querySelector("Button")
 
 	// document.querySelectorAll("Button")[1]
 	let solution = Number(LowV) + (Number(HighV) - Number(LowV)) * ((Number(knownAlterV) - Number(LowAlterV) ) / (Number(HighAlterV) - Number(LowAlterV)));
-	let sol = solution.toFixed(6);
+	let sol = solution.toFixed(10);
 	let x =document.createElement("div");
 	x.textContent = sol;
 	document.querySelectorAll("h3")[0].appendChild(x);
 
 	let quality = ((Number(knownAlterV) - Number(LowAlterV) ) / (Number(HighAlterV) - Number(LowAlterV)));
-	let qual = quality.toFixed(6);
+	let qual = quality.toFixed(10);
 	let y = document.createElement("div");
 	y.textContent = qual;
 	document.querySelectorAll("h3")[1].appendChild(y);
